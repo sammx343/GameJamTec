7285a381d3d1deb3d78de8c2020519238bcf8e1a
sammx343 <naitsabes29@gmail.com>
2021-04-17 16:15:39 -0500

    first commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D CharacterController2D;
+    public float runSpeed = 40f;
+    float horizontalMovement = 0f;
+    bool jump = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+	    if (Input.GetButtonDown("Jump"))
+	    {
+		    Debug.Log("IsJumping");
+		    jump = true;
+	    }
+    }
+
+    private void FixedUpdate()
+    {
+	    CharacterController2D.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+	    jump = false;
+    }
+}
